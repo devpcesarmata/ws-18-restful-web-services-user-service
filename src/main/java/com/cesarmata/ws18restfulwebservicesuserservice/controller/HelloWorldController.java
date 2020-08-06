9a69fe080584f0fd4b86df6ca7ec66f54351d451
César <dev.pcesarmata@gmail.com>
2020-08-05 20:21:49 -0500

    se concluye con la clase 20 del curso de udemy agregando el metodo POST.

--- src/main/java/com/cesarmata/ws18restfulwebservicesuserservice/controller/HelloWorldController.java
+++ src/main/java/com/cesarmata/ws18restfulwebservicesuserservice/controller/HelloWorldController.java
@@ -8,14 +8,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cesarmata.ws18restfulwebservicesuserservice.bean.HelloWorldBean;
 import com.cesarmata.ws18restfulwebservicesuserservice.dto.User;
 import com.cesarmata.ws18restfulwebservicesuserservice.service.UserService;
 
 /**
- * @author cesar
+ * @author Cesar Mata 
  *
  */
 @RestController
@@ -37,4 +38,11 @@ public class HelloWorldController {
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
+	
+	// POST /users/{id}{name}{birthday}
+	@PostMapping(path="/users")
+	public void createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+	}
+	
 }
